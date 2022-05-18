1bf44eea96a46f6f0704247b815046221c4a6ffd
Humberto Piva <contato.humbertopiva@gmail.com>
2022-05-18 19:23:18 -0300

    Adiciona Seção 1 - Aula 5

---
+++ inicio/app.js
@@ -0,0 +1,10 @@
+var mostrarSite = true;
+const site = "www.guiadoprogramador.com";
+
+console.log("Hello World");
+console.log("Meu nome é Humberto!");
+console.log("E eu estou aprendendo Node.js com o Guia do programador");
+
+if(mostrarSite) { // Se mostrar site
+    console.log(site) // Printe o site
+}
