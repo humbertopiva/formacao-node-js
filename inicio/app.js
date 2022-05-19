952cb851c55cdeee08585d6b44341e3468179403
Humberto Piva <contato.humbertopiva@gmail.com>
2022-05-18 21:40:07 -0300

    Adiciona código da Seção 1 - Aula 6

--- inicio/app.js
+++ inicio/app.js
@@ -1,10 +1,19 @@
-var mostrarSite = true;
-const site = "www.guiadoprogramador.com";
+var calculadora = require("./calculadora");
 
-console.log("Hello World");
-console.log("Meu nome é Humberto!");
-console.log("E eu estou aprendendo Node.js com o Guia do programador");
+console.log(calculadora.mult(10,20));
+console.log(calculadora.soma(20,40));
+console.log(calculadora.nome);
 
-if(mostrarSite) { // Se mostrar site
-    console.log(site) // Printe o site
-}
+
+
+
+// var mostrarSite = true;
+// const site = "www.guiadoprogramador.com";
+
+// console.log("Hello World");
+// console.log("Meu nome é Humberto!");
+// console.log("E eu estou aprendendo Node.js com o Guia do programador");
+
+// if(mostrarSite) { // Se mostrar site
+//     console.log(site) // Printe o site
+// }
