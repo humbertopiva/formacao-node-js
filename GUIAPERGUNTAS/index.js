4fd75d7c02289347f6ae1be679cecdb44cbd6db3
Humberto Piva <contato.humbertopiva@gmail.com>
2022-06-01 21:46:44 -0300

    Adiciona Aula 24

--- GUIAPERGUNTAS/index.js
+++ GUIAPERGUNTAS/index.js
@@ -4,8 +4,15 @@ const app = express(); // Iniciando o express
 // Estou dizendo para o Express usar o EJS como View engine
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.render("index")
+app.get("/:nome/:lang", (req, res) => {
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    res.render("index", {
+        nome: nome,
+        lang: lang,
+        empresa: "Guia do programador",
+        inscritos: 8040
+    });
 });
 
 app.listen(8080, () => {
