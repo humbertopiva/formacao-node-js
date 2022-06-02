306e03ef74a5f2604489a091d6b6bcaa0a3f08e3
Humberto Piva <contato.humbertopiva@gmail.com>
2022-06-01 21:34:31 -0300

    Adiciona Aula 23

---
+++ GUIAPERGUNTAS/index.js
@@ -0,0 +1,13 @@
+const express = require("express"); // Importando o express
+const app = express(); // Iniciando o express
+
+// Estou dizendo para o Express usar o EJS como View engine
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index")
+});
+
+app.listen(8080, () => {
+    console.log("App rodando!")
+});
