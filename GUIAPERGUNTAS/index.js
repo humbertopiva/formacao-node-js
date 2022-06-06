d81083d56f20516520629561eaf9c26957398a7d
Humberto Piva <contato.humbertopiva@gmail.com>
2022-06-06 20:26:21 -0300

    Adiciona Aula 26

--- GUIAPERGUNTAS/index.js
+++ GUIAPERGUNTAS/index.js
@@ -8,12 +8,25 @@ app.get("/:nome/:lang", (req, res) => {
     var nome = req.params.nome;
     var lang = req.params.lang;
     var exibirMsg = false;
+
+    var produtos = [
+        {nome: "Doritos", preco: 3.14},
+        {nome: "Coca-cola", preco: 5},
+        {nome: "Leite", preco: 1.45},
+        {nome: "Carne", preco: 15},
+        {nome: "Red Bull", preco: 6},
+        {nome: "Nescau", preco: 4},
+    ];
+
+    
+
     res.render("index", {
         nome: nome,
         lang: lang,
         empresa: "Guia do programador",
         inscritos: 8040,
-        msg: exibirMsg
+        msg: exibirMsg,
+        produtos: produtos
     });
 });
 
