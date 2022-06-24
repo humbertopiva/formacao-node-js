41f380bf7aa11272dd6a7126608cf324dec27f9c
Humberto Piva <contato.humbertopiva@gmail.com>
2022-06-24 01:33:30 -0300

    Adiciona aula 37

--- GUIAPERGUNTAS/index.js
+++ GUIAPERGUNTAS/index.js
@@ -1,18 +1,27 @@
 const express = require("express"); // Importando o express
 const app = express(); // Iniciando o express
+const bodyParser = require("body-parser");
 
 // Estou dizendo para o Express usar o EJS como View engine
 app.set("view engine", "ejs");
 app.use(express.static('public'));
-
+// Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+// Rotas
 app.get("/", (req, res) => {
-
     res.render("index");
 });
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar");
-})
+});
+
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário recebido! titulo " + titulo + " " + " descricao" + descricao);
+});
 
 app.listen(8080, () => {
     console.log("App rodando!");
