b9a9d9bbeb076de46a47ec9fc7375916ef44361a
Humberto Piva <contato.humbertopiva@gmail.com>
2023-04-19 22:26:12 -0300

    Adiciona módulo Consumo de API Rest com JWT

--- APIDEGAMES/index.js
+++ APIDEGAMES/index.js
@@ -46,7 +46,7 @@ var DB = {
 
 app.get("/games", auth, (req, res) => {
     res.statusCode = 200;
-    res.json({user: req.loggedUser, games: DB.games});
+    res.json(DB.games);
 })
 
 app.get("/game/:id", (req, res) => {
