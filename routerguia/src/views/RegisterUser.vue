385503a663e2b150d72ac02fbe4118e5fb796009
Humberto Piva <contato.humbertopiva@gmail.com>
2023-05-29 23:19:35 -0300

    Adiciona exemplo de Vue Router

---
+++ routerguia/src/views/RegisterUser.vue
@@ -0,0 +1,23 @@
+<template lang="">
+    <div>
+        <input type="text" placeholder="email"><br>
+        <input type="password" placeholder="password"><br>
+        <button>Send</button>
+        <h4>{{params}}</h4>
+    </div>
+</template>
+<script>
+export default {
+    created() {
+       this.params = this.$route.params;
+    },
+    data() {
+        return {
+            params: "Nada",
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
